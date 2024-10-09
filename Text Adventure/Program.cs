ba889a01ea7377716662c36e0d23a0f42d3ffcaa
MUKILTEO\1721890 <1721890@mukilteo.wednet.edu>
2024-10-09 08:57:09 -0700

    changed entire text advenuture, orginal idea to advanced for me

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,66 +1,47 @@
-﻿using System.Security.AccessControl;
+﻿using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 
 namespace Text_Adventure
 {
     internal class Program
     {
-        static void Main(string[] args)// this is an adventure into a dark cursed forest, where it will eventually loop back onto itself, mixing and warping reality everytime you make a full loop, or die.
-            //every time it loops, things get mixed up or scrambled, or stacked, like for instane there is an oak and birch tree mixed and jumbled together, that deer is croaking like a frog.
-            //you have to pay attention to the paths, the more things are warped, the stronger the forest, and its best to avoid going that route.
+        static void Main(string[] args)
         {
-            Console.WriteLine("You are in a dark forest, its getting dark and you have to get back, infront of you there are two paths. ");
-            Console.WriteLine(" both seem the same, but you notice footprints going right, which path Do you choose?");
+            Console.WriteLine("You are in a forest, its getting dark and you have to get back, infront of you there are two paths. ");
+            Console.WriteLine(" both seem the same, the left path goes deeper into the woods, and the right path takes you towards a lake, which path Do you choose?");
             Console.WriteLine("Left | Right");
             String input = Console.ReadLine();
-            if (input == "Left")//correct choice
+            if (input == "Left")
             {
-                
-                Console.WriteLine("You decide to not follow those prints, and move left, after a while of walking you see three paths.");
-                Console.WriteLine("On the left you see smoke, probably a campfire, on the Right you see a lake far ahead in the distance, and in the middle you see .... more forest, which do you choose?");
-                Console.WriteLine("Campfire | Middle | Lake");
+                Console.WriteLine("You take the left path, it takes you deeper into the woods, as you go down, you see some smoke coming up through the trees on the left, on the right, more woods.");
+                Console.WriteLine(" Left | Right");
                 input = Console.ReadLine();
-                if (input == "Campfire")//Campfire smoke
+                if (input == "Left")
                 {
-                    Console.WriteLine("You decide to follow the smoke and try to find survivors, but as you get closer you see the smoke start to get smaller and smaller, when you make it there, you find....nothing");
-                    Console.WriteLine("there was no camp, there was just an open clearing with a path continuing on into the woods, should you turn back, or head on into the woods?");//both paths return to the begining, loop 1
-                    Console.WriteLine("Reverse | Forward");
-                    input = Console.ReadLine();
-                    if (input == "Reverse")
-                    {
-                        Console.WriteLine("You decide to head turn back, but as your makeing you way back through the woods, things start to look...familer, and as you turn right into a dense part of the forest.");
-                        Console.WriteLine("You see...The same duel path as before, same trees, same rocks, same footprints, but now you recognize them, they are yours, but instead of them going right");
-                        Console.WriteLine("They are going left, you seem to have, looped back around somehow, but its getting darker and darker as time moves ever foward, you have two options, Left | Right");
-                        input = Console.ReadLine();
-                        if (input == "Right")
-                        {
-
-                        }
-                        else if (input == "Left")
-                        {
-
-                        }
-                    }
-                    else if (input == "Foward")
-                    {
-                        Console.WriteLine("You head foward, into the darkness ahead, you can make out some vague shapes in the dark, people standing behind trees, branchs breaking, but its getting harder to see");
-                        Console.WriteLine("after you walk around a bend you see...The duel paths you took at the beginning, with footprints going Left, your foot prints, and nothing going to the right this time.");
-                        Console.WriteLine("you once again need to make this choice, you seem to have looped around to the beggining of the paths, now you must choose the best path.  Left | Right");
-                    }
+                    Console.WriteLine("you follow the smoke and eventally, find a destroyed campsite, with claw and scratch marks everywhere, you turn around to find a pack of very angry wolves behind you.");
+                    Console.WriteLine("you are mauled to death by the pack");
                 }
-                else if (input == "Middle")//more forest
+                else if (input == "Right")
                 {
-                    Console.WriteLine("You head forward, deeper into the woods and as you walk you begin to notice things are looking familer...");
-                }
-                else if (input == "Lake")//lake
-                {
-                    Console.WriteLine("You head towards the lake, and after lots of walking, arrive to this great massive crystal clear lake, it is beutiful, but there doesnt seem to be a way back to you camp from here.");
-                    Console.WriteLine("you could stop to get some freshwater, or continue on around it, getting some fresh water for your travels could be useful. Drink | Continue");
+                    Console.WriteLine("You take the path that leads deeper into the woods, eventually find a clearing with a map on it, you can either go the long way back to your camp, or take a short route to a campsite nearby. what do you want to do?");
+                    Console.WriteLine("Short | Long");
                 }
             }
-            else if (input == "Right")//poor but still viable choice
+            else if (input == "Right")
             {
-                
-                Console.WriteLine("You decide, against your better judgement, to follow the foot prints.");
+                Console.WriteLine("You take the Right path, which heads towards the lake, but right before the lake, there is a path that splits off and goes left, what do you want to do?");
+                Console.WriteLine("Continue | Left");
+                input = Console.ReadLine();
+                if (input == "Continue")
+                {
+                    Console.WriteLine("You continue forward and manage to make it to the lake, it is full of fresh water, and there are some paths up ahead, do you want to stay and get some water, or continue?");
+                    Console.WriteLine("Stay | Foward");
+                }
+                else if (input == "Left")
+                {
+                    Console.WriteLine("You take the left path, and after a while, run into a mama bear, you try to escape, but you are attacked and mauled");
+                    Console.WriteLine("You die");
+                }
             }
         
         
