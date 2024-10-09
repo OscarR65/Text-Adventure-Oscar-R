6de2c8be7ccf40b9d66f80c00ab8be115fd3e975
MUKILTEO\1721890 <1721890@mukilteo.wednet.edu>
2024-10-09 09:07:45 -0700

    added stuff

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -8,7 +8,8 @@ namespace Text_Adventure
         static void Main(string[] args)
         {
             Console.WriteLine("You are in a forest, its getting dark and you have to get back, infront of you there are two paths. ");
-            Console.WriteLine(" both seem the same, the left path goes deeper into the woods, and the right path takes you towards a lake, which path Do you choose?");
+            Console.WriteLine(" both seem the same, the left path goes deeper into the woods, and the right path takes you towards a lake, ");
+            Console.WriteLine("which path Do you choose?");
             Console.WriteLine("Left | Right");
             String input = Console.ReadLine();
             if (input == "Left")
@@ -25,6 +26,15 @@ namespace Text_Adventure
                 {
                     Console.WriteLine("You take the path that leads deeper into the woods, eventually find a clearing with a map on it, you can either go the long way back to your camp, or take a short route to a campsite nearby. what do you want to do?");
                     Console.WriteLine("Short | Long");
+                    input = Console.ReadLine();
+                    if (input == "Short")
+                    {
+
+                    }
+                    else if (input == "Long")
+                    {
+
+                    }
                 }
             }
             else if (input == "Right")
@@ -36,6 +46,15 @@ namespace Text_Adventure
                 {
                     Console.WriteLine("You continue forward and manage to make it to the lake, it is full of fresh water, and there are some paths up ahead, do you want to stay and get some water, or continue?");
                     Console.WriteLine("Stay | Foward");
+                    input = Console.ReadLine();
+                    if (input == "Stay")
+                    {
+
+                    }
+                    else if (input == "Foward")
+                    {
+
+                    }
                 }
                 else if (input == "Left")
                 {
